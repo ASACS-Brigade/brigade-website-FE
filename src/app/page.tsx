99ab001f80f7013f51c113ab1906ebe91d7046e4
michycipher <michelleutomi@yahoo.com>
2026-06-10 17:42:24 +0100

    fix: deployment rendering error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import AboutPreview from "../components/sections/about-preview";
 import ArticlesPreview from "../components/sections/articles-preview";
 import CtaBanner from "../components/sections/cta-banner";
-import EventsPreview from "../components/sections/events-preview";
 import FeatureCards from "../components/sections/feature-cards";
 import GalleryPreview from "../components/sections/gallery-preview";
 import HomeHero from "../components/sections/home-hero";
-import Newsletter from "../components/sections/newsletter";
 import Programs from "../components/sections/programs";
 import Testimonials from "../components/sections/testimonials";
 
@@ -16,14 +14,12 @@ export default function HomePage() {
       <FeatureCards />
       <AboutPreview />
       <Programs />
-      {/* <EventsPreview /> */}
 
       <ArticlesPreview />
 
       <GalleryPreview />
 
       <Testimonials />
-      {/* <Newsletter /> */}
       <CtaBanner />
     </main>
   );
